ccef4e2fad470d54fa032432b3dc6e97c76c3df5
Hanien ElGalfy <helgalfy@gmail.com>
2020-01-10 21:33:39 +0200

    Tapping into the GPS

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController , CLLocationManagerDelegate{
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "4759ac9ac19e0b7a3bf8c5977b1ccd3a"
     
     let locationManager = CLLocationManager()
     //TODO: Declare instance variables here
@@ -32,8 +32,8 @@ class WeatherViewController: UIViewController , CLLocationManagerDelegate{
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation() //Asychronize method which it works in the background to try and grab the GPS location
         //TODO:Set up the location manager here.
-    
         
         
     }
@@ -77,14 +77,24 @@ class WeatherViewController: UIViewController , CLLocationManagerDelegate{
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            print ("longitude = \(location.coordinate.longitude) , latitude = \(location.coordinate.latitude)")
+            let longitude = String(location.coordinate.longitude)
+            let latitude = String(location.coordinate.latitude)
+            
+            let params : [String : String ] = ["lat" : latitude , "long" : longitude, "appid" : APP_ID]
+        }
+    }
     
     
     //Write the didFailWithError method here:
-    
-    
-    
-
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavaliable"
+    }
     
     //MARK: - Change City Delegate methods
     /***************************************************************/
